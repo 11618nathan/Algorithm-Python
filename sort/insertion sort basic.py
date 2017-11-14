7f449dd9d262632c3048e7603175b07117d4bd03
Baek Gi Chan <11618nathan@gmail.com>
2017-11-14 19:59:43 +0900

    Add files via upload

---
+++ sort/insertion sort basic.py
@@ -0,0 +1,19 @@
+
+# 삽입 정렬
+def insertion_sort(a):
+    n = len(a)
+
+    for i in range(1, n):
+        key = a[i]
+        j = i - 1
+
+        while j >= 0 and a[j] > key:
+            a[j + 1] = a[j]
+            j -= 1
+        a[j + 1] = key
+
+num = [7, 6, 8, 1, 16, 18, 9]
+
+insertion_sort(num)
+
+print(num)
