60b59194917edd01b018eb930b709cf4abcda91e
Baek Gi Chan <11618nathan@gmail.com>
2017-11-28 21:53:37 +0900

    Update factorial.py

--- factorial/factorial.py
+++ factorial/factorial.py
@@ -1,11 +1,11 @@
 
-def fact(n):
+def factorial(n):
     # 초기값
     s = 1
     
-    # 1 ~ 입력까지 
+    # 1 ~ 입력까지
     for i in range(1, n + 1):
         s = s * i
     return s
 
-print(fact(int(input('입  력: '))))
+print(factorial(int(input('입  력: '))))
