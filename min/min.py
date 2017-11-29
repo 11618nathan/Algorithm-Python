b7f13df54714da77d2a8f53be7850748eec1abbe
Baek Gi Chan <11618nathan@gmail.com>
2017-11-30 01:30:36 +0900

    Update min.py

--- min/min.py
+++ min/min.py
@@ -1,15 +1,16 @@
 
 # 최소값
-def min_n(n):
-    x = len(n)
-    min_first = n[0]
+def min_n(a):
+    x = len(a)
+
+    # 리스트 첫 번째 - 최대값
+    min_num = a[0]
 
     for i in range(1, x):
-        if n[i] < min_first:
-            min_first = n[i]
+        if a[i] > min_num:
+            min_num = a[i]
 
-    return min_first
+    return min_num
 
-a = [16, 18, 1, 6, 8, 33, 9]
-
-print(min_n(a))
+li = [16, 18, 1, 6, 8, 33, 9]
+print(min_n(li))
